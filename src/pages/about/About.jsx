e613c259d59ab5b92876cb7d85c712b9f43c2e33
Okoye Emeka <emekao9376@gmail.com>
2025-10-30 21:50:24 +0100

    update

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,14 @@ import React from "react"
 
 const About =()=>{
     return(
-        <div> About </div>
+        <div className="pt-20 h-screen  mx-auto w-[80%]"> 
+            <h1 className="text-3xl font-medium">
+                Casted! Publications
+            </h1>
+            <p className="mt-4 text-lg  ">
+                CASTED! is a student media publication that aims to tell the stories of students on campus. We platform experiences, amplify voices, and capture stories that matter. We’re not just ink on paper; we’re the megaphone for every student with something to say. Whether you’re penning an exposé on the state of waste disposal on campus (it’s bad) or sharing your epic adventure, CASTED! is your stage.
+            </p>
+        </div>
     )
 }
 
--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Blog =()=>{
     return(
-        <div className="mb-[20rem] mx-auto w-[80%]"> Blog</div>
+        <div className="pt-16 h-screen mx-auto w-[80%]"> Blog</div>
     )
 }
 
