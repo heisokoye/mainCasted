e5e679c2bcb13f3c52f716d2f9158057f4a65272
Okoye Emeka <emekao9376@gmail.com>
2025-10-30 14:43:14 +0100

    update

--- src/pages/home/components/hero/Hero.jsx
+++ src/pages/home/components/hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { BsArrowRight, BsArrowLeft } from 'react-icons/bs';
 import { motion, AnimatePresence } from 'framer-motion';
+import { Helmet } from "react-helmet";
 
 
 
@@ -93,71 +94,76 @@ const Hero = () => {
 
   return (
     <div className="w-full">
-        <section className='bg-[url("/ro.jpg")] bg-cover w-full h-[50vh] md:h-[70vh] lg:h-[70vh] '>
-            <div className='inset-0 absolute bg-black/70'></div>
-            <div ref={heroRef} className='flex md:justify-between items-center justify-center h-full relative z-10 mx-auto w-[80%]'>
-                <div>
-                    <motion.h1 className='hero-text w-[20rem]  text-3xl md:text-4xl md:w-[30rem] lg:text-5xl lg:w-[35rem] text-white font-medium  ' 
-                    initial={{ opacity: 0, y: -50 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 1.5, ease: 'easeInOut' }}>
-                        Discover the latest social events, gossips and more
-                    </motion.h1>
-                    <motion.button
-                        className='hero-text mt-4 bg-white text-amber-600 px-4 py-2 cursor-pointer rounded-lg shadow-lg flex items-center gap-2'
-                        initial={{ opacity: 0, y: 50 }}
+        <div>
+            <Helmet>
+                <link rel="preload" as="image" href="/ro.jpg" />
+            </Helmet>
+            <section className='bg-[url("/ro.jpg")] bg-cover w-full h-[50vh] md:h-[70vh] lg:h-[70vh] '>
+                <div className='inset-0 absolute bg-black/70'></div>
+                <div ref={heroRef} className='flex md:justify-between items-center justify-center h-full relative z-10 mx-auto w-[80%]'>
+                    <div>
+                        <motion.h1 className='hero-text w-[20rem]  text-3xl md:text-4xl md:w-[30rem] lg:text-5xl lg:w-[35rem] text-white font-medium  ' 
+                        initial={{ opacity: 0, y: -50 }}
                         animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1.5, ease: 'easeInOut' }}
-                        whileHover="hover"
-                        variants={{
-                            // This variant is implicitly used for the hover state
-                            hover: { scale: 1.05, transition: { type: 'spring', stiffness: 400, damping: 15 } }
-                        }}
-                    >
-                        <a href="https://whatsapp.com/channel/0029Vb56767I1rckrGeYrs1E" target='_blank' rel="noopener noreferrer" className='font-medium'>Subscribe</a>
-                        <motion.div variants={arrowVariants}><BsArrowRight /></motion.div>
-                    </motion.button>
-                </div>
+                        transition={{ duration: 1.5, ease: 'easeInOut' }}>
+                            Discover the latest social events, gossips and more
+                        </motion.h1>
+                        <motion.button
+                            className='hero-text mt-4 bg-white text-amber-600 px-4 py-2 cursor-pointer rounded-lg shadow-lg flex items-center gap-2'
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 1.5, ease: 'easeInOut' }}
+                            whileHover="hover"
+                            variants={{
+                                // This variant is implicitly used for the hover state
+                                hover: { scale: 1.05, transition: { type: 'spring', stiffness: 400, damping: 15 } }
+                            }}
+                        >
+                            <a href="https://whatsapp.com/channel/0029Vb56767I1rckrGeYrs1E" target='_blank' rel="noopener noreferrer" className='font-medium'>Subscribe</a>
+                            <motion.div variants={arrowVariants}><BsArrowRight /></motion.div>
+                        </motion.button>
+                    </div>
 
-                    {/*Slider for blogs preview */}
-                <div className='hidden lg:flex items-center justify-center'>
-                    <div ref={sliderRef} className="relative xl:w-[50rem] h-[30rem] rounded-lg shadow-2xl overflow-hidden">
-                        <AnimatePresence initial={false} custom={direction}>
-                            <motion.div
-                                key={currentSlide}
-                                custom={direction}
-                                variants={slideVariants}
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
-                                className="absolute w-full h-full"
-                            >
-                                <img src={preview[currentSlide].pictures} alt={preview[currentSlide].titles} className="w-full h-full object-cover"/>
-                                <motion.div 
-                                    className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black/70 to-transparent rounded-b-lg"
-                                    variants={textContainerVariants}
+                        {/*Slider for blogs preview */}
+                    <div className='hidden lg:flex items-center justify-center'>
+                        <div ref={sliderRef} className="relative xl:w-[50rem] h-[30rem] rounded-lg shadow-2xl overflow-hidden">
+                            <AnimatePresence initial={false} custom={direction}>
+                                <motion.div
+                                    key={currentSlide}
+                                    custom={direction}
+                                    variants={slideVariants}
                                     initial="hidden"
                                     animate="visible"
                                     exit="exit"
-                                    custom={direction}
+                                    className="absolute w-full h-full"
                                 >
-                                    <motion.h3 variants={textItemVariants} className="text-white text-xl font-semibold mb-2">{preview[currentSlide].titles}</motion.h3>
-                                    <motion.p variants={textItemVariants} className="text-gray-200">{preview[currentSlide].excerpt}</motion.p>
+                                    <img src={preview[currentSlide].pictures} alt={preview[currentSlide].titles} className="w-full h-full object-cover"/>
+                                    <motion.div 
+                                        className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black/70 to-transparent rounded-b-lg"
+                                        variants={textContainerVariants}
+                                        initial="hidden"
+                                        animate="visible"
+                                        exit="exit"
+                                        custom={direction}
+                                    >
+                                        <motion.h3 variants={textItemVariants} className="text-white text-xl font-semibold mb-2">{preview[currentSlide].titles}</motion.h3>
+                                        <motion.p variants={textItemVariants} className="text-gray-200">{preview[currentSlide].excerpt}</motion.p>
+                                    </motion.div>
                                 </motion.div>
-                            </motion.div>
-                        </AnimatePresence>
-                        {/* Left Arrow */}
-                        <div onClick={prevSlide} className='absolute top-[50%] -translate-y-1/2 left-2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer z-20'>
-                            <BsArrowLeft size={20} />
-                        </div>
-                        {/* Right Arrow */}
-                        <div onClick={nextSlide} className='absolute top-[50%] -translate-y-1/2 right-2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer z-20'>
-                            <BsArrowRight size={20}  />
+                            </AnimatePresence>
+                            {/* Left Arrow */}
+                            <div onClick={prevSlide} className='absolute top-[50%] -translate-y-1/2 left-2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer z-20'>
+                                <BsArrowLeft size={20} />
+                            </div>
+                            {/* Right Arrow */}
+                            <div onClick={nextSlide} className='absolute top-[50%] -translate-y-1/2 right-2 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer z-20'>
+                                <BsArrowRight size={20}  />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </div>
   );
 }
