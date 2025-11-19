9d1351454187ad1258b645aab45c20cc0d455622
Okoye Emeka <emekao9376@gmail.com>
2025-11-19 22:46:48 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Footer from "./components/footer/Footer";
 import ProtectedRoute from "./pages/admin/ProtectedRoute";
 import Loader from "./components/loader/Loader";
 import AnalyticsTracker from "./components/analytics/AnalyticsTracker";
+import Notifications from "./components/notifications/Notifications";
 
 // Eager-load Home for faster FCP/LCP on the landing route; keep others lazy
 import Home from "./pages/home/Home";
@@ -26,6 +27,7 @@ const App = () => {
     <HelmetProvider>
       <BrowserRouter>
         <AnalyticsTracker />
+        <Notifications />
         {/* Navbar appears on all pages */}
         <Navbar />
 
