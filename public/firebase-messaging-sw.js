2ec20a4a48ad601cac68cd051d82f65437fc9f21
Okoye Emeka <emekao9376@gmail.com>
2025-11-25 11:23:12 +0100

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage((payload) => {
     body,
     icon: icon || "/castedicon.png",
     badge: badge || "/castedicon.png",
-    image: image || "https://casted.blog/slider1.webp",
+    image: image || "/casted.webp",
     data: { url },
     tag: "casted-update", // prevents duplicate notifications
   };
